ff6bd18fa94f3b6639dfe0241b9053058aef7009
kenny <astrok100@users.noreply.github.com>
2017-08-13 22:12:50 +0100

    adding cors

--- deals/routes.py
+++ deals/routes.py
@@ -4,15 +4,17 @@ from flask import Flask
 from flask import Response
 from pymongo import MongoClient
 from bson import json_util
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 mongo_client = MongoClient(app.config.get("MONGO_DATABASE_URI"))
 mongo_database = app.config.get("MONGO_DATABASE")
 db = mongo_client[mongo_database]
 
 
-@app.route('/current')
+@app.route('/api/v1/current')
 def get_current_deals():
     now = datetime.now()
     query = {
