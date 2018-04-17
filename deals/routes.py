87f3b51c3c45496b44157b8a68cc3f9eb3a95fb9
kenny <astrok100@users.noreply.github.com>
2018-04-17 22:44:42 +0100

    commiting local changes, its been a while

--- deals/routes.py
+++ deals/routes.py
@@ -12,6 +12,7 @@ CORS(app, resources={r"/api/*": {"origins": "*"}})
 mongo_client = MongoClient(app.config.get("MONGO_DATABASE_URI"))
 mongo_database = app.config.get("MONGO_DATABASE")
 db = mongo_client[mongo_database]
+cache = None
 
 
 @app.route('/api/v1/current')
@@ -21,11 +22,27 @@ def get_current_deals():
         "date_from": {"$lte": now},
         "date_to": {"$gte": now}
     }
-
+    if cache:
+        data = cache
+    else:
+        data = {
+            "offers": [offer for offer in db['offers'].find(query)],
+            "promo": [promo for promo in db['offers'].distinct('promo')]
+        }
     return Response(
         json_util.dumps(
-            [offer for offer in db['offers'].find(query)],
+            data,
             json_options=json_util.STRICT_JSON_OPTIONS
+        ),
+        mimetype='application/json'
+    )
+
+
+@app.route('/api/v1/test')
+def test():
+    return Response(
+        json_util.dumps(
+            {}
         ),
         mimetype='application/json'
     )
--- run.py
+++ run.py
@@ -1,4 +1,21 @@
-from deals.routes import app 
+from deals.routes import app
 
-if __name__ == '__main__':
-    app.run()
+from tornado.wsgi import WSGIContainer
+from tornado.ioloop import IOLoop
+from tornado.web import FallbackHandler, RequestHandler, Application
+
+
+class MainHandler(RequestHandler):
+    def get(self):
+        self.write("This message comes from Tornado ^_^")
+
+tr = WSGIContainer(app)
+
+application = Application([
+    (r"/tornado", MainHandler),
+    (r".*", FallbackHandler, dict(fallback=tr)),
+])
+
+if __name__ == "__main__":
+    application.listen(5000)
+    IOLoop.instance().start()
